50fe335457ebb269a40c7ca6168dda4d7b6ccb3d
Volodymyr Shevchuk <dosandkv@gmail.com>
2026-08-14 16:00:54 +0300

    feat: update vitest configs for cli and functions

--- cli/vitest.config.ts
+++ cli/vitest.config.ts
@@ -14,6 +14,15 @@ export default defineConfig({
   },
   test: {
     include: ['src/**/*.spec.ts'],
-    environment: 'node'
+    environment: 'node',
+    coverage: {
+      provider: 'v8',
+      thresholds: {
+        lines: 90,
+        branches: 85,
+        functions: 90,
+        statements: 90
+      }
+    }
   }
 });
--- functions/vitest.config.js
+++ functions/vitest.config.js
@@ -4,6 +4,15 @@ export default defineConfig({
   test: {
     include: ['src/**/*.spec.{js,ts}'],
     exclude: [...configDefaults.exclude, 'src/**/*.i.spec.ts'],
-    environment: 'node'
+    environment: 'node',
+    coverage: {
+      provider: 'v8',
+      thresholds: {
+        lines: 90,
+        branches: 85,
+        functions: 90,
+        statements: 90
+      }
+    }
   }
 });
